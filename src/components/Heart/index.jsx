e716d9b2aa81d6f297adcaeb493187265c776cfa
castthecalmingapple <kyletucci@gmail.com>
2024-01-02 13:58:51 -0500

    Swapped out nes heart icons for fa-icons so I can fill them different colors for different debts

--- src/components/Heart/index.jsx
+++ src/components/Heart/index.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartOutline } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+
 const Heart = ({ isFilled }) => {
   return (
-    <i
-      id={crypto.randomUUID()}
-      className={`nes-icon is-small heart ${isFilled ? "" : "is-empty"}`}
-    ></i>
+    <FontAwesomeIcon
+      className={`heart-icon ${isFilled ? "filled" : ""}`}
+      icon={isFilled ? faHeartSolid : faHeartOutline}
+    />
   );
 };
 
